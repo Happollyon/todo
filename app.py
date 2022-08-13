e3ed7651de87437248a0ce1e927d23e2401e284d
Fagner Nunes <fagnernunes11@gmail.com>
2022-08-14 00:23:23 +0100

    fixed bugs

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from shutil import unregister_unpack_format
-from sre_constants import SUCCESS
-from urllib import request
+import re
+from urllib import response
 from flask import Flask, jsonify,request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session,sessionmaker
+from flask_cors import CORS
 
 app = Flask(__name__)
+
 engine = create_engine("postgresql://ftrxafhanwopfs:4799a0f535c4c402e226523afefbbecfc2f3a3fc85bfce8c3013b9d589758a2d@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/d1hk19qr73vntu")
 db = scoped_session(sessionmaker(bind=engine))
 #index page
@@ -20,11 +21,14 @@ def createTable(query):
     print('SUCCESS!')
 
 
-@app.route('/register', methods = ['POST'])
-def register():
-    username = request.get_json(force=True)
-    print(username['username'])
-    return jsonify({'username':username['username']})
+@app.route('/register/<string:username>/<string:password>',methods=['GET'])
+def register(username,password):
+    result = db.execute("INSERT INTO users(username,password) VALUES(:username,:password) RETURNING user_id, username",{'username':username,'password':password}).fetchall()
+    db.commit()
+    print(result)
+    response = jsonify({'response':username})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return  response
 
 if __name__ == '__main__':
     app.run(debug=True)
